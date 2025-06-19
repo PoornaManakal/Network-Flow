af8e43e0477890186f53f67f6f75d31b3c8cbe50
PoornaManakal <166126968+PoornaManakal@users.noreply.github.com>
2025-06-20 02:31:38 +0530

    edge class created

---
+++ Edge.java
@@ -0,0 +1,17 @@
+public class Edge {
+    int from, to;
+    int capacity;
+    int flow;
+
+    public Edge(int from, int to, int capacity) {
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0; // Initially no flow
+    }
+
+    public int residualCapacity() {
+        return capacity - flow;
+    }
+}
+
